98522aeeb55f13f12d077b576786e493758c11eb
Lilian Azevedo <lilian.azevedo0905@gmail.com>
2022-08-23 10:25:50 -0300

    feat: create new action for lint default export

--- front-end/src/redux/actions/index.js
+++ front-end/src/redux/actions/index.js
@@ -1,8 +1,14 @@
-import { SET_USER_INFOS } from './actionTypes';
+import { DISABLE_BUTTON, SET_USER_INFOS } from './actionTypes';
 
 export const saveUser = (user) => (
   {
     type: SET_USER_INFOS,
     payload: user,
+  }
+);
+
+export const disableButton = () => (
+  {
+    type: DISABLE_BUTTON,
   }
 );
--- front-end/src/redux/reducers/userReducer.js
+++ front-end/src/redux/reducers/userReducer.js
@@ -5,7 +5,7 @@ const INITIAL_STATE = { email: '' };
 const userReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
   case SET_USER_INFOS:
-    return { ...state, user: { email: action.payload } };
+    return { ...state, email: action.payload };
   default:
     return state;
   }
