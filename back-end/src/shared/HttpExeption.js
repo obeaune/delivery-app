d4d6b3f67d4a943f7c5b089f59a81968b1431a40
Brenno Miliani <brennomiliani@hotmail.com>
2022-08-23 17:10:57 -0300

    fix: fixing lint error

--- back-end/src/shared/HttpExeption.js
+++ back-end/src/shared/HttpExeption.js
@@ -1,10 +1,8 @@
 class HttpException extends Error {
-  status;
-
   constructor(status, message) {
     super(message);
     this.status = status;
   }
 }
 
-export default HttpException;
+module.exports = HttpException;
