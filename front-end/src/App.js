90c591833c7f159dc8d0c7b57cb746512fedb057
fernando <pcabral.fernando@gmail.com>
2022-08-23 14:46:07 -0300

    comitando para dar o pull

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,29 +1,15 @@
-// import React from 'react';
-// import { Route, Switch } from 'react-router-dom';
-
-// function App() {
-//   return (
-//     <Switch>
-//       <Route exact path="/" component={ Home } />
-//       <Route path="/create" component={ SignIn } />
-//       <Route path="/login" component={ Login } />
-//       <Route path="*" component={ NotFound } />
-//     </Switch>);
-// }
-
-// export default App;
 import React from 'react';
-import './App.css';
-import rockGlass from './images/rockGlass.svg';
+import { Route, Switch } from 'react-dom';
+import Home from './pages/Home';
 
 function App() {
   return (
-    <div className="App">
-      <span className="logo">TRYBE</span>
-      <object className="rocksGlass" type="image/svg+xml" data={ rockGlass }>
-        Glass
-      </object>
-    </div>
+    <Switch>
+      <Route exact path="/" component={ Home } />
+      {/* <Route path="/create" component={ SignIn } />
+      <Route path="/login" component={ Login } />
+      <Route path="*" component={ NotFound } /> */}
+    </Switch>
   );
 }
 
---
+++ front-end/src/pages/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import rockGlass from '../images/rockGlass.svg';
+
+function App() {
+  return (
+    <div className="App">
+      <span className="logo">TRYBE</span>
+      <object className="rocksGlass" type="image/svg+xml" data={ rockGlass }>
+        Glass
+      </object>
+    </div>
+  );
+}
+
+export default App;
