54dbf3a12658fc5efbe46d0cb49862455531fb8e
Lilian Azevedo <lilian.azevedo0905@gmail.com>
2022-08-22 22:17:31 -0300

    fix: issues linter

--- front-end/src/pages/register.js
+++ front-end/src/pages/register.js
@@ -5,28 +5,27 @@ const INITIAL_STATE = {
   inputName: '',
   inputEmail: '',
   inputPassword: '',
-  inputValueInitial: '',
-}
+};
 
-const SignIn = () => {
+function SignIn() {
   const [userData, setUserData] = useState(INITIAL_STATE);
   // const history = useHistory();
 
   const handleInput = ({ target: { name, value } }) => {
-    setUserData(prev => ({...prev, [name]: value }));
-  }
+    setUserData((prev) => ({ ...prev, [name]: value }));
+  };
 
-  const { inputName, inputEmail, inputPassword, inputValueInitial } = userData;
+  const { inputName, inputEmail, inputPassword } = userData;
 
   return (
     <div className="general-page">
       <div>
-        <h1 className='main-title home-main-title'>LogoAqui</h1>
+        <h1 className="main-title home-main-title">LogoAqui</h1>
       </div>
       <div>
-        <h1 className='h1-title'>Crie a sua conta!</h1>
+        <h1 className="h1-title">Crie a sua conta!</h1>
       </div>
-  
+
       <section>
         <input
           type="text"
@@ -49,26 +48,26 @@ const SignIn = () => {
           name="inputPassword"
           placeholder="Senha (mínimo 6 caracteres)"
         />
-        </section>
+      </section>
 
-        <div className='section-btns'>
-          <button
-              type="button"
-              className='button-general button--flex'
-              // onClick={ handleClick }
-          >
+      <div className="section-btns">
+        <button
+          type="button"
+          className="button-general button--flex"
+          // onClick={ handleClick }
+        >
           Cadastrar
-          </button>
-          {/* <button
+        </button>
+        {/* <button
               type="button"
               className='button-general button--flex'
               // onClick={ () => history.push('/') }
           >
           Voltar
           </button> */}
-        </div>
+      </div>
     </div>
   );
-};
+}
 
 export default SignIn;
