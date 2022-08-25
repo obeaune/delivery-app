380251a5251c853d973aa100ece3a6a3d13bdaa8
Lilian Azevedo <lilian.azevedo0905@gmail.com>
2022-08-25 18:28:07 -0300

    fix: add cors connection

---
+++ front-end/src/components/navBar.js
@@ -0,0 +1,54 @@
+// import React, { useState } from 'react';
+// import convertedValue from '../services/utils';
+
+// function NavBar() {
+//   const [valueAdd, setValueAdd] = useState(0);
+
+//   return (
+//     <nav className="nav container">
+//       <div className="nav_menu" id="nav-menu">
+//         <ul className="nav_list grid">
+//           <li className="nav_item" onClick={ addNavBar }>
+//             <Link to="/login" />
+//             Trocar Usuário
+//           </li>
+
+//           <li className="nav_item" onClick={ addNavBar }>
+//             <Link to="/account">
+//               <Unicons.UilMoneyWithdraw size="30" color="#E4B600" />
+//             </Link>
+//             Depósito/
+//             Retirada
+//           </li>
+
+//           <li className="nav_item" onClick={ addNavBar }>
+//             <Link to="/wallet">
+//               <Unicons.UilWallet size="30" color="#E4B600" />
+//             </Link>
+//             Carteira de Ações
+//           </li>
+
+//           <li className="nav_item" onClick={ addNavBar }>
+//             <Link to="/">
+//               <Unicons.UilSignOutAlt size="30" color="#E4B600" />
+//             </Link>
+//             Sair
+//           </li>
+
+//           <li className="nav_item" onClick={ addNavBar }>
+//             <Link to="/historic">
+//               <Unicons.UilHistory size="30" color="#E4B600" />
+//             </Link>
+//             Extrato
+//           </li>
+
+//           <li onClick={ addNavBar } className="nav_item nav_close">
+//             <Unicons.UilMultiply size="30" color="#E4B600" />
+//           </li>
+//         </ul>
+//       </div>
+//     </nav>
+//   );
+// }
+
+// export default NavBar;
