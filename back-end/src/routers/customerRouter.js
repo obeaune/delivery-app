95d086d71c5091b9f3b4090c768969241922309d
obeaune <linecora86@gmail.com>
2022-08-29 19:24:34 -0300

    feat: create the route to `costumer/orders/:id`

--- back-end/src/routers/customerRouter.js
+++ back-end/src/routers/customerRouter.js
@@ -1,9 +1,12 @@
 const { Router } = require('express');
 
 const CustomerController = require('../controllers/CustomerController');
+const authenticationMiddleware = require('../middlewares/authMiddleware');
 
 const router = Router();
 
 router.get('/products', CustomerController.getAll);
+
+router.get('/orders/:id', authenticationMiddleware, CustomerController.getById);
 
 module.exports = router;
