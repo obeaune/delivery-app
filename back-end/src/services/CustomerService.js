815c97966a533533e34d0685ea159517214c5803
obeaune <linecora86@gmail.com>
2022-08-30 15:41:33 -0300

    test

--- back-end/src/services/CustomerService.js
+++ back-end/src/services/CustomerService.js
@@ -16,42 +16,6 @@ const getOrderById = async ({ id }) => {
   return sale;
 };
 
-// the new return ---->
-
-// {
-//   "id": 1,
-//   "userId": 3,
-//   "sellerId": 2,
-//   "totalPrice": "4.69",
-//   "deliveryAddress": "Rua Cachaça 51, Bairro Tequila",
-//   "deliveryNumber": "+553199120-2020",
-//   "saleDate": "2022-08-01T19:58:00.000Z",
-//   "status": "Pendente",
-//   "seller": {
-//       "name": "Fulana Pereira"
-//   },
-//   "products": [
-//       {
-//           "id": 1,
-//           "name": "Skol Lata 250ml",
-//           "price": "2.20",
-//           "url_image": "http://localhost:3001/images/skol_lata_350ml.jpg",
-//           "SaleProduct": {
-//               "quantity": 1
-//           }
-//       },
-//       {
-//           "id": 3,
-//           "name": "Antarctica Pilsen 300ml",
-//           "price": "2.49",
-//           "url_image": "http://localhost:3001/images/antarctica_pilsen_300ml.jpg",
-//           "SaleProduct": {
-//               "quantity": 1
-//           }
-//       }
-//   ]
-// }
-
 module.exports = {
   getAll,
   getOrderById,
