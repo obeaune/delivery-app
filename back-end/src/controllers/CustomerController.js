251053d2c4e5c453d98726a64dfaceb5ab16301d
Brenno Miliani <brennomiliani@hotmail.com>
2022-08-30 14:57:44 -0300

    fixing merge

--- back-end/src/controllers/CustomerController.js
+++ back-end/src/controllers/CustomerController.js
@@ -6,22 +6,19 @@ const getAll = async (_req, res) => {
   return res.status(StatusCodes.OK).json(result);
 };
 
-<<<<<<< HEAD
 const checkout = async (req, res) => {
   const id = await customerService.checkout(req.body);
   return res.status(StatusCodes.OK).json({ id });
 };
 
-module.exports = {
-  getAll,
-  checkout,
-};
-=======
 const getAllOrdersByClient = async (_req, res) => {
   const { email } = res.locals.payload;
   const result = await customerService.getAllOrdersByClient(email);
   return res.status(StatusCodes.OK).json(result);
 };
 
-module.exports = { getAll, getAllOrdersByClient };
->>>>>>> cb929ab9859dc5bc4b7fd7193a733ddc825c2578
+module.exports = {
+  getAll,
+  checkout,
+  getAllOrdersByClient,
+};
