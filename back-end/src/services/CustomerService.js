a4892e59e409a29a5e8e7b44105da59d983ad974
obeaune <linecora86@gmail.com>
2022-08-29 23:07:59 -0300

    fix: change `customerService`

--- back-end/src/services/CustomerService.js
+++ back-end/src/services/CustomerService.js
@@ -10,17 +10,47 @@ const getAll = async () => {
 const getOrderById = async ({ id }) => {
   const isValidSale = await Sale.findOne({ where: { id } });
   if (!isValidSale) throw new HttpException(StatusCodes.NOT_FOUND, 'Sale Not Found');
-
-  const { dataValues: { sellerId } } = await Sale.findOne({ where: { id } });
-  const { name } = await User.findOne({ where: sellerId });
   const sale = await Sale.findOne({ where: { id },
-    include: { model: User, as: 'seller', attributes: ['name'] },
-  });
-
-  // until next modification
-  sale.seller.name = name;
+    include: [{ model: User, as: 'seller', attributes: ['name'] },
+    { model: Product, as: 'products', through: { attributes: ['quantity'] } }] });
   return sale;
 };
+
+// the new return ---->
+
+// {
+//   "id": 1,
+//   "userId": 3,
+//   "sellerId": 2,
+//   "totalPrice": "4.69",
+//   "deliveryAddress": "Rua Cachaça 51, Bairro Tequila",
+//   "deliveryNumber": "+553199120-2020",
+//   "saleDate": "2022-08-01T19:58:00.000Z",
+//   "status": "Pendente",
+//   "seller": {
+//       "name": "Fulana Pereira"
+//   },
+//   "products": [
+//       {
+//           "id": 1,
+//           "name": "Skol Lata 250ml",
+//           "price": "2.20",
+//           "url_image": "http://localhost:3001/images/skol_lata_350ml.jpg",
+//           "SaleProduct": {
+//               "quantity": 1
+//           }
+//       },
+//       {
+//           "id": 3,
+//           "name": "Antarctica Pilsen 300ml",
+//           "price": "2.49",
+//           "url_image": "http://localhost:3001/images/antarctica_pilsen_300ml.jpg",
+//           "SaleProduct": {
+//               "quantity": 1
+//           }
+//       }
+//   ]
+// }
 
 module.exports = {
   getAll,
