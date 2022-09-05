fa87b036e20a835c90adb73eb40bb3070df7911c
Brenno Miliani <brennomiliani@hotmail.com>
2022-09-05 15:55:25 -0300

    feat: created a route to update the status of an order

--- back-end/src/routers/sellerRouter.js
+++ back-end/src/routers/sellerRouter.js
@@ -7,5 +7,6 @@ const router = Router();
 
 router.get('/', authenticationMiddleware, SellerController.getAllSales);
 router.get('/:id', authenticationMiddleware, SellerController.findBySale);
+router.patch('/:id', authenticationMiddleware, SellerController.updateSaleStatus);
 
 module.exports = router;
